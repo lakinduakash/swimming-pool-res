f6d6fab94b82e009cbc16ee436a9af3728653ce6
lakinduakash <lakinduakash@gmail.com>
2018-11-13 03:44:41 +0530

    Add firebase and add configs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyDQRvcYdTMgos0fZDwcniA7M55x0hUyt9I",
+    authDomain: "swimming-pool-res.firebaseapp.com",
+    databaseURL: "https://swimming-pool-res.firebaseio.com",
+    projectId: "swimming-pool-res",
+    storageBucket: "swimming-pool-res.appspot.com",
+    messagingSenderId: "668286675302"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyDQRvcYdTMgos0fZDwcniA7M55x0hUyt9I",
+    authDomain: "swimming-pool-res.firebaseapp.com",
+    databaseURL: "https://swimming-pool-res.firebaseio.com",
+    projectId: "swimming-pool-res",
+    storageBucket: "swimming-pool-res.appspot.com",
+    messagingSenderId: "668286675302"
+  }
 };
 
 /*
