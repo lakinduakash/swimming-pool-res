6d573004c21b3a43d6a6a3ba1eace04d542b530f
lakinduakash <lakinduakash@gmail.com>
2018-11-22 02:28:35 +0530

    add authGuard to provider array

--- src/app/core/auth/auth.module.ts
+++ src/app/core/auth/auth.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {AuthService} from "./auth.service";
+import {AuthGuard} from './auth.guard';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [],
-  providers:[AuthService]
+  providers:[AuthService,AuthGuard]
 })
 export class AuthModule { }
