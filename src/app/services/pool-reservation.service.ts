a5082161a6870c379c68611ddec01d5edd87f66c
lakinduakash <lakinduakash@gmail.com>
2018-11-24 03:55:09 +0530

    Update services

--- src/app/services/pool-reservation.service.ts
+++ src/app/services/pool-reservation.service.ts
@@ -1,10 +1,16 @@
 import {Injectable} from '@angular/core';
+import {AngularFirestore} from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PoolReservationService {
 
-  constructor() {
+  constructor(public firestore: AngularFirestore) {
+  }
+
+  // example for save event to firestore. This method can called from any component
+  addEvent(event) {
+    this.firestore.collection(`events/${event.month}`).add(event);
   }
 }
