5befe79e8991a6772739e72008bffa397fc2595e
lakinduakash <lakinduakash@gmail.com>
2018-11-25 00:56:17 +0530

    Add login and logout functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from './core/auth/auth.service';
 import {MatDialog} from '@angular/material';
 import {LoginComponent} from './signup-login/login/login.component';
@@ -8,18 +8,46 @@ import {LoginComponent} from './signup-login/login/login.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'swimming-pool-res';
+
+  loginText = 'Login/Signup';
 
   constructor(public authService: AuthService, public dialog: MatDialog) {
 
   }
 
-  openLoginDialog() {
+  ngOnInit() {
+
+    this.authService.user.subscribe(
+      user => {
+        console.log(user);
+        if (user != null) {
+          this.loginText = 'Hi, ' + user.displayName;
+        } else {
+          this.loginText = 'Login/Signup';
+        }
+      }
+    );
+
+  }
+
+  login() {
 
     let dialogRef;
-    if (this.dialog.openDialogs.length === 0) {
-      dialogRef = this.dialog.open(LoginComponent, {minWidth: '330px'});
-    }
+    const sub = this.authService.user.subscribe(
+      user => {
+        if (user == null) {
+
+          if (this.dialog.openDialogs.length === 0) {
+            dialogRef = this.dialog.open(LoginComponent, {minWidth: '330px'});
+          }
+        } else {
+          dialogRef.close();
+          sub.unsubscribe();
+        }
+      }
+    );
+
   }
 }
