85b8bd070719bb1f4c44a7110d2259096632df22
lakinduakash <lakinduakash@gmail.com>
2018-11-26 00:07:40 +0530

    Add footer component to module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ButtonWaterComponent} from './button-water/button-water.component';
+import {FooterComponent} from './footer/footer.component';
 
 @NgModule({
-  declarations: [ButtonWaterComponent],
+  declarations: [ButtonWaterComponent, FooterComponent],
   imports: [
     CommonModule
   ],
-  exports: [ButtonWaterComponent]
+  exports: [ButtonWaterComponent, FooterComponent]
 })
 export class SharedModule {
 }
