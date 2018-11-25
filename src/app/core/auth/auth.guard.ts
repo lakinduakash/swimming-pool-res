74bbffae629f5ee30eb387d5dd88c757a3902329
lakinduakash <lakinduakash@gmail.com>
2018-11-26 04:42:07 +0530

    Add redirection on login fail

--- src/app/core/auth/auth.guard.ts
+++ src/app/core/auth/auth.guard.ts
@@ -21,7 +21,7 @@ export class AuthGuard implements CanActivate {
       tap(loggedIn => {
         if (!loggedIn) {
           console.log('access denied');
-          // this.router.navigate(['/login']);
+          this.router.navigate(['/home']);
         }
       })
   )
