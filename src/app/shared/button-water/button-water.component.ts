1a9774a133c35b332a8b772d27227b289cd5a551
lakinduakash <lakinduakash@gmail.com>
2018-11-26 01:37:11 +0530

    Add customization to button

--- src/app/shared/button-water/button-water.component.ts
+++ src/app/shared/button-water/button-water.component.ts
@@ -8,7 +8,10 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 export class ButtonWaterComponent implements OnInit {
 
   @Input('text')
-  text: string;
+  buttonText: string;
+
+  @Input('width')
+  width = '50px';
   @Output('clickButton')
   click: EventEmitter<any> = new EventEmitter<any>();
 
