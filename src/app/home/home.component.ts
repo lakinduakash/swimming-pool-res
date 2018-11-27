78a651c01df8832dc52a8c6dbdf6cac3c839c3d2
lakinduakash <lakinduakash@gmail.com>
2018-11-28 03:37:18 +0530

    Add more responsiveness

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {Router} from "@angular/router";
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +10,8 @@ export class HomeComponent implements OnInit {
 
   title = "Aqua Technics";
 
+  buttonText = 'Login To Reserve';
+
   constructor(private router: Router) {}
 
   ngOnInit() {
@@ -19,5 +21,6 @@ export class HomeComponent implements OnInit {
   redirectUser(){
     this.router.navigate(['login']);
   }
+
 
 }
